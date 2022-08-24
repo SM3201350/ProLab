b4e77f6278e388a6a01b5e5cdefe1a8be6e9db26
AnnaDiGio <103442632+SM3201350@users.noreply.github.com>
2022-08-24 14:54:42 +0000

    Esercizio_4

---
+++ Esercizio_4.py
@@ -0,0 +1,41 @@
+class CSVfile():
+
+    def __init__(self,name):
+
+        self.name = name
+
+    def __str__(self):
+
+        return 'CSVfile "{}".'.format(self.name)
+
+    def get_data(self):
+
+        values = []
+
+        the_file = open(self.name,'r')
+        
+        for line in the_file:
+
+            #faccio lo split di ogni riga sulla virgola
+            elements = line.split(',')
+
+            #se NON sto processando l'intestazione...
+            if elements[0] != 'Date':
+
+                #setto la data e il valore
+                date = elements[0]
+                value = elements[1]
+
+                #aggiungo alla lista dei valori questo valore
+                values.append(value)
+
+        print(values)
+        the_file.close()
+
+
+
+
+
+my_file = CSVfile('shampoo_sales.csv')
+print (my_file)
+my_file.get_data()
