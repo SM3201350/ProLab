58ff8c1e2898c83887b258a418f2b24044256e9c
AnnaDiGio <103442632+SM3201350@users.noreply.github.com>
2022-09-10 15:41:39 +0000

    backup 17:41

--- Esame/esame.py
+++ Esame/esame.py
@@ -23,9 +23,48 @@ class CSVTimeSeriesFile:
             print('ok')
         # se non riesco alzo un'eccezione
         except ExamException as e:
-            print('Errore in apertura del file: "{}"'.format(e))
+            print('Errore! File non aperto o illeggibile: "{}"'.format(e))
+
+        #inizializzo una lista dove salvare i dati del file
+        data=[]
+
+        #apro il file
+        my_file=open(self.name,'r')
+
+        #leggo ogni riga del file
+        for line in my_file:
+
+            #faccio lo split sulla virgola 
+            elements=line.split(',')
+
+            if elements[0]!= 'epoch':
+
+                data.append(elements)
+                
+
+
+        #chiudo il file
+        my_file.close()
+
+        return data
+                    
+
+        
+
+    
+            
             
 
 
+
+
+
+
+
+            
+#---------------------------------------------------
+#          Corpo del programma
+#---------------------------------------------------
 time_series_file=CSVTimeSeriesFile(name='data.csv')
 time_series=time_series_file.get_data()
+print(time_series)
