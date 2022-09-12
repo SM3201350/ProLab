e85de577bf40aec13956dab0727fb38a3c5836ee
AnnaDiGio <103442632+SM3201350@users.noreply.github.com>
2022-09-12 15:04:27 +0000

    backup 17:04 del 12/9

--- Esame/esame.py
+++ Esame/esame.py
@@ -1,13 +1,13 @@
-#  Eccezioni da alzare in caso ci fossero problemi
+#----------------------------------------------
+#            Eccezioni da alzare 
+#----------------------------------------------
 class ExamException(Exception):
 
         pass
 
-
-
-#--------------------------------
-#        classe principale
-#--------------------------------
+#-----------------------------------------------
+#   classe per leggere e convertire un file
+#-----------------------------------------------
 
 class CSVTimeSeriesFile :
 
@@ -21,12 +21,13 @@ class CSVTimeSeriesFile :
         #provo ad aprire il file
         try:
             my_file = open(self.name,'r')
+            
         #alzo eccezione se non riesco
         except Exception as e:
             raise ExamException('Errore! File non leggibile: {}'.format(e))
 
 
-        #inizializzo la lista data per salvare i dati in formato numerico
+        #inizializzo la lista "data" per salvare i dati in formato numerico
         data=[]
 
         #apro il file
@@ -48,7 +49,7 @@ class CSVTimeSeriesFile :
                     elements[0]=int(elements[0])
                 #se non riesco gli assegno None e coninuo fino alla fine della lista
                 except:
-                    elements[0]=0
+                    elements[0]=None
                     continue
 
                 #provo a convertire la temperatura in valore numerico
@@ -56,7 +57,7 @@ class CSVTimeSeriesFile :
                     elements[1]=float(elements[1])
                 #se non riesco gli assegno None e coninuo fino alla fine della lista
                 except:
-                    elements[1]=0
+                    elements[1]=None
                     continue
 
                 #aggiungo alla lista i nuovi valori convertiti
@@ -67,12 +68,27 @@ class CSVTimeSeriesFile :
 
         return data
 
+#-------------------------------------------------------------------------
+#     Funzione controlla se i giorni sono ordinati e senza ripetizioni
+#-------------------------------------------------------------------------
+def check_list(time_series):
 
+    for i in range(len(time_series)):
+        if(time_series[i][0]==time_series[i-1][0]):
+            time_series[i][0]=None
+
+    return time_series
+
+
+
+
+
+        
 #------------------------------------------------
-#              Funzione
+#     Funzione calcolo escursione giornaliera
 #------------------------------------------------
 def compute_daily_max_difference(time_series):
-
+    time_series=check_list(time_series)
     #istanzio la lista dove memorizzo le escursioni termiche
     temperature_range=[]
     #istanzio una variabile che conta i giorni
@@ -84,6 +100,7 @@ def compute_daily_max_difference(time_series):
     #itero 
     for i in range(len(time_series)):
 
+       
         if i==0:
             min=time_series[0][1]
             max=time_series[0][1]
@@ -95,25 +112,31 @@ def compute_daily_max_difference(time_series):
             elif(max<time_series[i][1]):
                 max=time_series[i][1]
 
-                
+            
         #calcolo il giorno alla mezzanotte precisa
         day_start_epoch=(time_series[index][0])-((time_series[index][0])%86400)
         
-        #verificp quando cambia il giorno
+        #verifico quando cambia il giorno
         #se epoch attuale sottratto all'inizio del giorno supera i secondi fdi un giorno intero
-        if(time_series[i][0]-day_start_epoch>=86400):
-            #cambia il giorno
-            giorno+=1
+        if(time_series[i][0]-day_start_epoch>=86400 or i==(len(time_series))-1):
             
             #assegno a index l'indice della lista in cui cambia giorno
             index=i
+            
+            
             difference=max-min
-            min=time_series[i][1]
-            max=time_series[i][1]
+            
             if(difference==0):
-                temperature_range.append(None)
+                temperature_range.append('------------')
             else:
                 temperature_range.append(difference)
+
+            print('giorno_{}  , {}'.format(giorno,temperature_range[giorno-1]))
+            
+            min=time_series[i][1]
+            max=time_series[i][1]
+            #cambia il giorno
+            giorno+=1
             
             
 
@@ -131,14 +154,7 @@ def compute_daily_max_difference(time_series):
 
 time_series_file=CSVTimeSeriesFile (name='data.csv')
 time_series=time_series_file.get_data()
-prova=compute_daily_max_difference(time_series)
-for i,value in enumerate(prova):
-    print('giorno_{} = {}'.format(i,value))
 
-
-
-
-
-
+compute_daily_max_difference(time_series)
 
 
